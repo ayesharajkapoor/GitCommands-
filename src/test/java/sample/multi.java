b14a85d05697cc2806548b57bd9ccd5964f7c017
ayesharajkapoor <ayesharajkapoor@gmail.com>
2024-12-24 12:11:21 +0530

    firsr commit

---
+++ src/test/java/sample/multi.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class multi {
+
+	public void mul()
+
+	{
+
+		System.out.println("multiply number 1,2");
+
+	}
+
+	public void div()
+
+	{
+
+		System.out.println("dividing number 1,2");
+
+	}
+
+}
